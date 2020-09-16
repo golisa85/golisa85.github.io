5c75f3de3573d18f970f276df73cc27bc93196a3
Minah Lim <minahlim@Minahs-MacBook-Pro.local>
2020-09-16 20:42:49 +0800

    Updates

---
+++ precache-manifest.75a005a9a52653b2a39a35b251a9a4d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933ff81a2a58fcbcc870b1d656c52500",
+    "url": "/golisa85.github.io/index.html"
+  },
+  {
+    "revision": "b59070a5fde456e251a6",
+    "url": "/golisa85.github.io/static/css/main.8f15540d.chunk.css"
+  },
+  {
+    "revision": "b3c2bc37b7ffd0560bc1",
+    "url": "/golisa85.github.io/static/js/2.2e6bab85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/golisa85.github.io/static/js/2.2e6bab85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59070a5fde456e251a6",
+    "url": "/golisa85.github.io/static/js/main.fce58421.chunk.js"
+  },
+  {
+    "revision": "bd761debc313a4bc73f3",
+    "url": "/golisa85.github.io/static/js/runtime-main.b530edc3.js"
+  },
+  {
+    "revision": "edaf6bd3b96468161a8027413d5b2877",
+    "url": "/golisa85.github.io/static/media/Picture 1.edaf6bd3.png"
+  }
+]);
